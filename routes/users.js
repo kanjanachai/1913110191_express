e87a1efaf2645e7f29f8c9c86954d86f80b89166
kanjanachai <lin.14525@gmail.com>
2022-11-26 11:25:43 +0700

    Workshop A

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(200).json({
+    fullname: 'Kanjanachai Yaowarat'
+  })
+});
+
+router.get('/bio', function(req, res, next) {
+  res.status(200).json({
+    fullname: 'Kanjanachai Yaowarat',
+    nickname: 'Lin',
+    hobby: 'sleep',
+    gitusername: 'kanjanachai'
+  })
 });
 
 module.exports = router;
