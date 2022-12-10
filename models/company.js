98ae5c5220827e2d7cb7d4240b747aa0926454ae
kanjanachai <lin.14525@gmail.com>
2022-12-10 11:39:12 +0700

    WorkShop A

--- models/company.js
+++ models/company.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const companySchema = new Schema({
-    name:  String, // String is shorthand for {type: String}
+const companySchema = new Schema(
+  {
+    name: String, // String is shorthand for {type: String}
     address: {
       province: String,
-    }
-  });
+    },
+  },
+  { collection: "setting" }
+);
 
-  const company = mongoose.model("Companys", companySchema);
+const company = mongoose.model("Companys", companySchema);
 
-  module.exports = company
+module.exports = company;
