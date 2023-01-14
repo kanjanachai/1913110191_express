e8fa1104b58b5a26b73299075bea8910992c8f80
kanjanachai <lin.14525@gmail.com>
2023-01-14 10:28:05 +0700

    encryptPassword

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 exports.index = (req, res, next) => {
   res.status(200).json({
     fullname: "Kanjanachai Yaowarat",
@@ -10,5 +12,20 @@ exports.bio = (req, res, next) => {
     nickname: "Lin",
     hobby: "sleep",
     gitusername: "kanjanachai",
+  });
+};
+
+exports.register = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  let user = new User();
+  user.name = name;
+  user.email = email;
+  user.password = await user.encryptPassword(password)
+
+  await user.save();
+
+  res.status(201).json({
+    message: "ลงทะเบียนเรียบร้อยแล้ว",
   });
 };
